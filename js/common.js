bc1a2a5af7f52750fd306d4b11c42a11cd7cf25e
Sanghyun Lee <tkdguslee92@gmail.com>
2023-04-16 21:33:35 +0900

    [이상현] 맥스가 만든 코드랑 병합 완료

---
+++ js/common.js
@@ -0,0 +1,6 @@
+//햄버거메뉴
+
+function menu() {
+  let slideMenu = document.querySelector(".slidemenu");
+  slideMenu.classList.toggle("active");
+}
---
+++ js/indexpage.js
@@ -0,0 +1,6 @@
+//컨텐츠개수 카운팅
+
+const amount = document.getElementsByClassName("useful_contents").length;
+console.log(amount);
+
+document.getElementsByClassName("counta")[0].innerHTML = amount;
